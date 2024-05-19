fd01d1bb80683675da25f2e2c3435f20dc9b6df6
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-19 16:48:22 +0300

    add authenticated component to check user

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,16 @@
+import { AppContext } from '../context/AppContext';
+import Authenticated from '../hoc/Authenticated';
+import { useContext } from 'react';
+
 export default function Home() {
+  const { userData } = useContext(AppContext);
   return (
     <>
       <div>
-        <p>Home</p>
+        <h1>Home</h1>
+        <Authenticated>
+          <p>Hello USER DATA: {userData?.username}</p>
+        </Authenticated>
       </div>
     </>
   );
