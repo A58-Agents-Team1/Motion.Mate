4504ec3ac30bb1522844d0e4dc452a4bbc0e2905
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-19 14:33:57 +0300

    Add Daisy UI to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <div className='App'>
+        <button className='btn'>Hello daisyUI</button>
+      </div>
     </>
   );
 }
