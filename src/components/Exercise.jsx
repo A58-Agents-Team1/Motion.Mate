66154548755d5ee94f391eed0bb30aa22ecca0f0
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-21 23:33:44 +0300

    Add functionality to each Exercise button "Join now"

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,60 +1,78 @@
 import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 import { getExercises } from '../services/exercise.service';
-import { CreateExercise } from './CreateExercise';
 import { onChildChanged, ref } from 'firebase/database';
 import { db } from '../config/firebase-config';
+import { ExerciseCategory } from './Exercise.Category';
+import { AccountStats } from './Account.Stats';
 
 export const Exercise = () => {
-  const { userData } = useContext(AppContext);
-
   const [allPosts, setAllPosts] = useState([]);
-
-  const getAllExercises = async () => {
-    const result = await getExercises();
-    setAllPosts(result);
-  };
-  getAllExercises();
+  const [selectedCategory, setSelectedCategory] = useState(false);
+  const [filteredCategory, setFilteredCategory] = useState(null);
 
   useEffect(() => {
-    return onChildChanged(ref(db, 'exercises'), (snapshot) => {
-      const value = snapshot.val();
-      setAllPosts((allPosts) =>
-        allPosts.map((exercise) => {
-          return exercise;
-        })
-      );
-    });
-  }, []);
+    getExercises().then(setAllPosts);
+    // console.log(selectedCategory);
+  }, [selectedCategory]);
+
+  //   useEffect(() => {
+  //   return onChildChanged(ref(db, 'exercises'), (snapshot) => {
+  //     const value = snapshot.val();
+  //     setAllPosts((allPosts) =>
+  //       allPosts.map((exercise) => {
+  //         return exercise;
+  //       })
+  //     );
+  //   });
+  // }, []);
+
+  const handleBackToMain = () => {
+    setSelectedCategory(false);
+  };
 
   return (
     <>
-      <CreateExercise />
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 '>
-        {allPosts.length > 0 &&
-          allPosts.map((post) => (
-            <div
-              className='card w-64 bg-base-100 shadow-xl image-full'
-              key={post.id}
-            >
-              <figure>
-                <img src={`src/assets/${post.title}.jpg`} alt='Shoes' />
-              </figure>
-              <div className='card-body'>
-                <h2 className='card-title'>{post.title}</h2>
-                <p>{post.duration}</p>
-                <p>{post.shortDescription}</p>
+      <AccountStats />
+      {selectedCategory === false && (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4'>
+          {allPosts.length > 0 &&
+            allPosts.map((post) => (
+              <div
+                className='card w-64 bg-base-100 shadow-xl image-full'
+                key={post.id}
+              >
+                <figure>
+                  <img src={`src/assets/${post?.category}.jpg`} alt='Shoes' />
+                </figure>
+                <div className='card-body'>
+                  <h2 className='card-title'>{post.category}</h2>
+                  <p>{post.title}</p>
+                  <p>{post.shortDescription}</p>
 
-                <div className='card-actions justify-end'>
-                  <button className='btn btn-primary'>Join Now</button>
+                  <button
+                    onClick={() => setSelectedCategory(post.category)}
+                    className='btn btn-primary'
+                  >
+                    Join Now
+                  </button>
                 </div>
-                <p className='text-primary uppercase bg-neutral-600/70'>
-                  Level : {post.level}
-                </p>
+                <p>Level : {post.level}</p>
               </div>
-            </div>
-          ))}
-      </div>
+            ))}
+        </div>
+      )}
+
+      {selectedCategory && (
+        <div>
+          <h1>Selected Category: {selectedCategory}</h1>
+          <ExerciseCategory
+            category={selectedCategory}
+            onBackToMain={handleBackToMain}
+            allPosts={allPosts}
+          />
+        </div>
+      )}
     </>
   );
 };
