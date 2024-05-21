0ef2839fa56e2ab37e84ca232388fb4d9fe1439c
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-21 23:30:54 +0300

    Add property category to the exercise component. Make the button a popping modal

--- src/components/CreateExercise.jsx
+++ src/components/CreateExercise.jsx
@@ -5,6 +5,7 @@ import { addUserExercise } from '../services/users.service';
 
 export const CreateExercise = () => {
   const [content, setContent] = useState({
+    category: '',
     title: '',
     duration: '',
     description: '',
@@ -13,7 +14,6 @@ export const CreateExercise = () => {
     shortDescription: '',
   });
   const { userData } = useContext(AppContext);
-  const [show, setShow] = useState(false);
 
   const updateContent = (e, prop) => {
     setContent({
@@ -24,6 +24,7 @@ export const CreateExercise = () => {
 
   const submitExercise = async () => {
     const result = await createExercises(
+      content.category,
       content.title,
       content.duration,
       content.description,
@@ -31,89 +32,115 @@ export const CreateExercise = () => {
       userData?.username,
       content.shortDescription
     );
-    setContent({});
+    setContent({
+      category: '',
+      title: '',
+      duration: '',
+      description: '',
+      level: '',
+      createdBy: '',
+      shortDescription: '',
+    });
     console.log(result);
 
     await addUserExercise(userData.username, result);
     console.log(content.createdBy);
-    setShow(!show);
   };
 
   return (
-    <div className=' flex justify-end'>
-      <div className='flex gap-36 '>
-        <button className='btn btn-outline btn-secondary '>Set Goals</button>
-        <button
-          onClick={() => setShow(!show)}
-          className='btn btn-outline btn-primary '
-        >
-          Create exercise
-        </button>
-      </div>
+    <div>
+      <button
+        className='btn btn-outline btn-primary'
+        onClick={() =>
+          document.getElementById('exercise-create-modal').showModal()
+        }
+      >
+        Create Exercise
+      </button>
+      <div>
+        <dialog id='exercise-create-modal' className='modal'>
+          <div className='modal-box'>
+            <h3 className='font-bold text-lg mb-3'>Ready to workout</h3>
 
-      {show && (
-        <div className=''>
-          <label className='input input-bordered flex items-center gap-2 select-primary'>
-            <input
-              onChange={(e) => updateContent(e.target.value, 'title')}
-              type='text'
-              className='grow'
-              placeholder='Title'
-              value={content.title}
-            />
-          </label>
-          <label className='input input-bordered flex items-center gap-2 select-primary'>
-            <input
-              onChange={(e) => updateContent(e.target.value, 'duration')}
-              type='text'
-              className='grow'
-              placeholder='Duration'
-              value={content.duration}
-            />
-          </label>
-          <label className='input input-bordered flex items-center gap-2 select-primary'>
-            <input
-              onChange={(e) => updateContent(e.target.value, 'description')}
-              type='text'
-              className='grow'
-              placeholder='description'
-              value={content.description}
-            />
-          </label>
-          <label className='input input-bordered flex items-center gap-2 select-primary'>
-            <input
-              onChange={(e) => updateContent(e.target.value, 'level')}
-              type='text'
-              className='grow'
-              placeholder='Level'
-              value={content.level}
-            />
-          </label>
-          <label className='input input-bordered flex items-center gap-2 select-primary'>
-            <input
-              onChange={(e) =>
-                updateContent(e.target.value, 'shortDescription')
-              }
-              type='text'
-              className='grow'
-              placeholder='Short description'
-              value={content.shortDescription}
-            />
-          </label>
-          <button
-            onClick={submitExercise}
-            className='btn btn-outline btn-primary'
-          >
-            Submit
-          </button>
-          {/* <button
-            onClick={setShow(!show)}
-            className='btn btn-outline btn-secondary'
-          >
-            Cancel
-          </button> */}
-        </div>
-      )}
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) => updateContent(e.target.value, 'category')}
+                type='text'
+                className='grow'
+                placeholder='Category'
+                value={content.category}
+              />
+            </label>
+
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) => updateContent(e.target.value, 'title')}
+                type='text'
+                className='grow'
+                placeholder='Title'
+                value={content.title}
+              />
+            </label>
+
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) => updateContent(e.target.value, 'duration')}
+                type='text'
+                className='grow'
+                placeholder='Duration'
+                value={content.duration}
+              />
+            </label>
+
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) => updateContent(e.target.value, 'description')}
+                type='text'
+                className='grow'
+                placeholder='description'
+                value={content.description}
+              />
+            </label>
+
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) => updateContent(e.target.value, 'level')}
+                type='text'
+                className='grow'
+                placeholder='Level'
+                value={content.level}
+              />
+            </label>
+
+            <label className='input input-bordered flex items-center gap-2 select-primary'>
+              <input
+                onChange={(e) =>
+                  updateContent(e.target.value, 'shortDescription')
+                }
+                type='text'
+                className='grow'
+                placeholder='Short description'
+                value={content.shortDescription}
+              />
+            </label>
+
+            <form method='dialog'>
+              <button
+                onClick={submitExercise}
+                className='btn btn-outline btn-primary'
+              >
+                Submit
+              </button>
+            </form>
+
+            <div className='modal-action'>
+              <form method='dialog'>
+                <button className='btn btn-outline btn-primary'>Cancel</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </div>
   );
 };
