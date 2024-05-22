f7e1dea5340ebf7546fce157508bdc2b037b4c0b
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-22 23:29:02 +0300

    Filtered out current user from user list in rendering SingleUserView.

--- src/views/AllUsers.jsx
+++ src/views/AllUsers.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { getAllUsers } from '../services/users.service';
 import SingleUserView from '../components/SingleUserView';
+import { AppContext } from '../context/AppContext';
 
 export default function AllUsers() {
+  const { userData } = useContext(AppContext);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -15,11 +17,13 @@ export default function AllUsers() {
     <div className='flex flex-col'>
       <div>
         <ul>
-          {users.map((user) => (
-            <li key={user.username}>
-              <SingleUserView user={user} />
-            </li>
-          ))}
+          {users
+            .filter((user) => user.username !== userData.username)
+            .map((user) => (
+              <li key={user.username}>
+                <SingleUserView user={user} />
+              </li>
+            ))}
         </ul>
       </div>
     </div>
