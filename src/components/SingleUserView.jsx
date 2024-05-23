9b7ac9db78961ad38d04c8cda4a8d81b49b504e3
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-23 12:21:23 +0300

    Import an Avatar component to avoid rewriting code and do View Profile Functionality

--- src/components/SingleUserView.jsx
+++ src/components/SingleUserView.jsx
@@ -1,26 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import userPhoto from '../assets/userPhoto.png';
+import Avatar from './Avatar';
 
 export default function SingleUserView({ user }) {
+  const navigate = useNavigate();
+
   return (
-    <div className='card card-side shadow-xl m-4 border border-gray-400 h-70 w-full'>
-      <figure>
-        {user?.avatar ? (
-          <img
-            src={user?.avatar}
-            title='Profile Photo'
-            alt='userPhoto'
-            className='w-80 h-full fill-current'
-          />
-        ) : (
-          <img
-            src={userPhoto}
-            alt='userPhoto'
-            title='Missing Profile Photo'
-            className='w-80 h-full fill-current'
-          />
-        )}
-      </figure>
+    <div className='card card-side shadow-xl m-4 border border-gray-400 rounded-2xl h-70 w-full'>
+      <Avatar user={user} />
       <div className='card-body text-center'>
         {user?.firstName ? (
           <h2 className='card-title'>{`${user?.firstName} ${user?.lastName}`}</h2>
@@ -31,7 +18,12 @@ export default function SingleUserView({ user }) {
         <p></p>
         <div className='card-actions justify-end'>
           <button className='btn btn-primary'>Add Friend</button>
-          <button className='btn btn-primary'>View Profile</button>
+          <button
+            onClick={() => navigate(`/users/${user?.username}`)}
+            className='btn btn-primary'
+          >
+            View Profile
+          </button>
         </div>
       </div>
     </div>
