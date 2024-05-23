fcbb971a370ee67d3b644cdb352410f25b675e3f
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-23 14:18:26 +0300

    remove unnecessary import and component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,11 @@
-import { AccountStats } from '../components/Account.Stats';
-import { Exercise } from '../components/Exercise';
+import { Exercise } from '../components/Exercise/Exercise';
 import { AppContext } from '../context/AppContext';
 import { useContext } from 'react';
 
 export default function Home() {
-  const { userData } = useContext(AppContext);
   return (
     <>
       <div className='flex flex-col gap-5'>
-        <AccountStats />
         <Exercise />
       </div>
     </>
