77268804dad51b244b02481c5bd9157980c21d93
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-24 16:40:18 +0300

    Add constants for validation parameters

--- src/common/constants.js
+++ src/common/constants.js
@@ -7,3 +7,13 @@ export const APP_NAME = 'Motion Mate';
 export const GOAL_MIN_NAME_LENGTH = 3;
 
 export const GOAL_MAX_NAME_LENGTH = 30;
+
+export const MIN_USERNAME_LENGTH = 2;
+
+export const MAX_USERNAME_LENGTH = 20;
+
+export const PHONE_NUMBER_LENGTH = 10;
+
+export const MIN_PASSWORD_LENGTH = 6;
+
+export const MAX_PASSWORD_LENGTH = 15;
