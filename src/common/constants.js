942f2d49bc8225392b6e400d9170bf98417c3234
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-24 04:21:52 +0300

    Add APP_NAME GOAL_MIN_NAME_LENGTH GOAL_MAX_NAME_LENGTH

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,9 @@
 export const LIGHT_THEME = 'wireframe';
 
 export const DARK_THEME = 'business';
+
+export const APP_NAME = 'Motion Mate';
+
+export const GOAL_MIN_NAME_LENGTH = 3;
+
+export const GOAL_MAX_NAME_LENGTH = 30;
