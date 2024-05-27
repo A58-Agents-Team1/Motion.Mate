35a2325005b73fe334e682f7a037a9a00af94245
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-27 19:27:40 +0300

    Add AlertHelper

---
+++ src/helper/alert-helper.js
@@ -0,0 +1,8 @@
+export const alertHelper = (setMessage, setAlert, message) => {
+  setMessage(message);
+  setAlert(true);
+  setTimeout(() => {
+    setAlert(false);
+    setMessage(null);
+  }, 3000);
+};
