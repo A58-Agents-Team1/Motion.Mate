ab10874e5beff698a12c7a1270c1e0995daa1f83
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-27 23:00:26 +0300

    validate the create exercise form

---
+++ src/common/exercise.validations.js
@@ -0,0 +1,45 @@
+import {
+  EXERCISE_MAX_CONTENT_LENGTH,
+  EXERCISE_MAX_TITLE_LENGTH,
+  EXERCISE_MIN_CONTENT_LENGTH,
+  EXERCISE_MIN_TITLE_LENGTH,
+} from './constants';
+
+export const validateExerciseForm = (
+  title,
+  content,
+  hours,
+  minutes,
+  seconds,
+  calories,
+  level,
+  category
+) => {
+  if (!title) {
+    throw new Error('title is required');
+  }
+  if (title.length < EXERCISE_MIN_TITLE_LENGTH) {
+    throw new Error('title is too short');
+  }
+  if (title.length > EXERCISE_MAX_TITLE_LENGTH) {
+    throw new Error('title is too long');
+  }
+  if (content.length < EXERCISE_MIN_CONTENT_LENGTH) {
+    throw new Error('content is too short');
+  }
+  if (content.length > EXERCISE_MAX_CONTENT_LENGTH) {
+    throw new Error('content is too long');
+  }
+  if (!hours && !minutes && !seconds) {
+    throw new Error('missing time duration for exercise');
+  }
+  if (!calories) {
+    throw new Error('please add calories');
+  }
+  if (!level) {
+    throw new Error('please select a level');
+  }
+  if (!category) {
+    throw new Error('please select a category');
+  }
+};
