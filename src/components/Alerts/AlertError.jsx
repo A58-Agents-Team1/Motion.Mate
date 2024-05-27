c9c4e3da4ad1a426e9b74dc2468d8e01302bb921
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-27 14:40:29 +0300

    Add AlertError, AlertSuccess

---
+++ src/components/Alerts/AlertError.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+const AlertError = ({ message }) => {
+  return (
+    <div className='toast toast-end z-50'>
+      <div className='alert alert-error'>{message}</div>
+    </div>
+  );
+};
+
+AlertError.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+export default AlertError;
---
+++ src/components/Alerts/AlertSuccess.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+const AlertSuccess = ({ message }) => {
+  return (
+    <div className='toast toast-end z-50'>
+      <div className='alert alert-success'>
+        <span>{message}</span>
+      </div>
+    </div>
+  );
+};
+
+AlertSuccess.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+export default AlertSuccess;
