50098720ec6d15ca5505cd0bdca2ad1760840cb0
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-29 13:54:23 +0300

    Add views for logged user and not logged

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,110 +1,135 @@
 import { useEffect, useState } from 'react';
 import { getAllUsers } from '../services/users.service';
 import { getExercises } from '../services/exercise.service';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { HomeAuthenticated } from './HomeAuthenticated';
+import { useNavigate } from 'react-router-dom';
+import { getAllCategories } from '../services/category.service';
+import { Carousel } from '../components/Home/Caroulsel';
 
 export default function Home() {
   const [allUsers, setAllUsers] = useState([]);
   const [allExercises, setAllExercises] = useState([]);
+  const { userData } = useContext(AppContext);
+  const [allCategories, setAllCategories] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const func = async () => {
       const result = await getAllUsers();
+      const exercises = await getExercises();
       const users = result.val();
+      const categories = await getAllCategories();
+      setAllCategories(categories.length);
       setAllUsers(Object.keys(users).length);
-      const snapshot = await getExercises();
-      setAllExercises(Object.keys(snapshot).length);
+      setAllExercises(Object.keys(exercises).length);
     };
     func();
   }, []);
 
   return (
-    <div className='flex flex-col'>
-      <div className=' bg-base-200'>
-        <div className='hero-content flex-col lg:flex-row'>
-          <img
-            src='https://i.pinimg.com/564x/34/28/86/34288601de8af9fe8aaa9d11c4a56db1.jpg'
-            className='max-w-sm rounded-lg shadow-2xl'
-          />
-          <div>
-            <h1 className='text-5xl font-bold'>Ready to join the lifestyle?</h1>
-            <p className='py-6'>
-              Find the right category that fits you. Challenge yourself with
-              exercises with difficulty for beginners or lovers. Here you can
-              find all type of exercises for all parts of the body, find other
-              users and exchange ideas.
-            </p>
-            <button className='btn btn-secondary'>Get Started</button>
+    <div>
+      {userData?.username ? (
+        <HomeAuthenticated />
+      ) : (
+        <div className='flex flex-col'>
+          <div className=' bg-base-200'>
+            <div className='hero-content flex-col lg:flex-row'>
+              <Carousel></Carousel>
+              <div>
+                <h1 className='text-5xl font-bold'>
+                  Ready to join the lifestyle?
+                </h1>
+                <p className='py-6'>
+                  Find the right category that fits you. Challenge yourself with
+                  exercises with difficulty for beginners or lovers. Here you
+                  can find all type of exercises for all parts of the body, find
+                  other users and exchange ideas.
+                </p>
+                <button
+                  onClick={() => navigate('/login')}
+                  className='btn btn-secondary'
+                >
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='max-w-fit'>
+            <div className='stats shadow flex-1'>
+              <div className='stat'>
+                <div className='stat-figure text-secondary'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    className='inline-block w-8 h-8 stroke-current'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+                    ></path>
+                  </svg>
+                </div>
+                <div className='stat-title'>Category exercises</div>
+                <div className='stat-value text-secondary'>
+                  {allCategories || 0}
+                </div>
+                <div className='stat-desc'>Create your own personal </div>
+              </div>
+
+              <div className='stat'>
+                <div className='stat-figure text-secondary'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    className='inline-block w-8 h-8 stroke-current'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
+                    ></path>
+                  </svg>
+                </div>
+                <div className='stat-title'>All Users</div>
+                <div className='stat-value text-secondary'>{allUsers || 0}</div>
+                <div className='stat-desc'>
+                  ↗︎ What are you waiting for - join us!
+                </div>
+              </div>
+
+              <div className='stat'>
+                <div className='stat-figure text-secondary'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    className='inline-block w-8 h-8 stroke-current'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4'
+                    ></path>
+                  </svg>
+                </div>
+                <div className='stat-title'>Exercises available</div>
+                <div className='stat-value text-secondary mb-1'>
+                  {allExercises || 0}
+                </div>
+                <div className='stat-desc'>Be active</div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='max-w-fit'>
-        <div className='stats shadow flex-1'>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block w-8 h-8 stroke-current'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                ></path>
-              </svg>
-            </div>
-            <div className='stat-title'>Downloads</div>
-            <div className='stat-value text-secondary'>0</div>
-            <div className='stat-desc'>Jan 1st - Feb 1st</div>
-          </div>
-
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block w-8 h-8 stroke-current'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
-                ></path>
-              </svg>
-            </div>
-            <div className='stat-title'>All Users</div>
-            <div className='stat-value text-secondary'>{allUsers}</div>
-            <div className='stat-desc'>
-              ↗︎ What are you waiting for - join us!
-            </div>
-          </div>
-
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block w-8 h-8 stroke-current'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4'
-                ></path>
-              </svg>
-            </div>
-            <div className='stat-title'>All exercises available</div>
-            <div className='stat-value text-secondary'>{allExercises}</div>
-            <div className='stat-desc'>Be active</div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ src/views/HomeAuthenticated.jsx
@@ -0,0 +1,10 @@
+import { AccountStats } from '../components/Exercise/AccountStats';
+import { Divider } from '../components/HomeAuth/Divider';
+
+export const HomeAuthenticated = () => {
+  return (
+    <div>
+      <AccountStats />
+    </div>
+  );
+};
