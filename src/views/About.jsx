7888998be204d5795613f5749f928be0b8afd45a
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-29 11:17:41 +0300

    change the view of About

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getAllUsers } from '../services/users.service';
-import backgroundUsers from '../assets/backgroundUsers.png';
+import { SVG } from '../components/About/SVG';
 
 export default function About() {
   const [countUser, setCountUser] = useState();
@@ -12,20 +12,80 @@ export default function About() {
   }, [countUser]);
 
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className='font-bold text-4xl mb-3'>About</h1>
-      <div className='card h-40 w-60 bg-base-100 shadow-xl image-full rounded-3xl'>
-        <figure>
-          <img src={backgroundUsers} alt='Users' />
-        </figure>
-        <div className='card-body'>
-          <h2 className='card-title underline'>Registered Users</h2>
-          <p>
-            Currently, there are <strong>{countUser}</strong> registered users.
-            👤
-          </p>
-        </div>
+    <ul className='timeline timeline-snap-icon max-md:timeline-compact timeline-vertical'>
+      <div className='text-lg font-black text-center mb-3 text-primary'>
+        MOTION MATE
       </div>
-    </div>
+      <li>
+        <div className='timeline-middle'>
+          <SVG />
+        </div>
+        <div className='timeline-start md:text-end mb-10 text-lg font-black'>
+          This is your personal fitness app, designed for users to set their own
+          goals. With a simple and intuitive design, we make every workout not a
+          duty, but a habit, thats drives you out of bed! We encourage people to
+          be active and feel energized.
+        </div>
+        <hr className='bg-primary' />
+      </li>
+      <li>
+        <hr className='bg-primary' />
+        <div className='timeline-middle'>
+          <SVG />
+        </div>
+        <div className='timeline-end mb-10 text-lg font-black'>
+          <div className='text-lg font-black text-primary'>Why choose us?</div>
+          Skip the everyday calculations of how much calories you have burned.
+          Here you can calculate your daily calories burned during workout.
+          Also, you don`t need to search in the web to find your perfect
+          exercise videos. Make your list here, where you have access to them
+          non-stop and at any time you can add more exercises in your session,
+          divide them in different categories for better user experience.
+        </div>
+        <hr className='bg-primary' />
+      </li>
+      <li>
+        <hr className='bg-primary' />
+        <div className='timeline-middle'>
+          <SVG />
+        </div>
+        <div className='timeline-start md:text-end mb-10 text-lg font-black'>
+          <div className='text-lg font-black text-primary'>
+            BMI, goals and more
+          </div>
+          We provide you with Body Mass Index tool to assess whether an
+          individual has a healthy body weight for a given height. <br /> You
+          can also set different personal goals with a time period. For example,
+          set a goal to be active all seven days of the week and more on!
+        </div>
+        <hr className='bg-primary' />
+      </li>
+      <li>
+        <hr className='bg-primary' />
+        <div className='timeline-middle'>
+          <SVG />
+        </div>
+        <div className='timeline-end mb-10 text-lg font-black'>
+          <div className='text-lg font-black text-primary'>Motion Mates</div>
+          Connect with people using the same app! <br />
+          You can add other users in your friend list and share exercises.
+          <br /> You decide which is your perfect Motion Mate!
+        </div>
+        <hr className='bg-primary' />
+      </li>
+      <li>
+        <hr className='bg-primary' />
+        <div className='timeline-middle'>
+          <SVG />
+        </div>
+        <div className='timeline-start md:text-end mb-10 text-lg font-black'>
+          <div className='text-lg font-black text-primary'>
+            So what are you waiting for ?
+          </div>
+          We are currently {countUser} users, and we are looking for the next
+          active people to join us!
+        </div>
+      </li>
+    </ul>
   );
 }
