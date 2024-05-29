4ef96b8660ad07e3cb85614b452a6868ff84a8c0
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-29 12:41:32 +0300

    Change view: ContactUs Login EditProfileInfo

--- src/views/ContactUs.jsx
+++ src/views/ContactUs.jsx
@@ -1,71 +1,84 @@
+import logo from '../assets/logo.png';
+import userPhoto from '../assets/userPhoto.png';
+
 export default function ContactUs() {
   return (
     <div className='flex flex-col items-center'>
-      <h1 className='font-bold text-4xl'>Contact Us</h1>
+      <h1 className='font-bold text-4xl'>Contact Us:</h1>
       <div className='flex flex-row my-6'>
-        <div id='Tisho'>
+        <div id='Tihomir'>
           <div className='flex flex-col gap-4 w-52 mx-4'>
             <div className='flex gap-4 items-center'>
-              <div className='skeleton w-16 h-16 rounded-full shrink-2'></div>
+              <div className='skeleton w-18 h-18 rounded-full shrink-2'>
+                <img src={userPhoto} />
+              </div>
               <div className='flex flex-col gap-4'>
                 <div className='skeleton text-center h-7 w-20'>Tihomir</div>
                 <div className='skeleton text-center h-7 w-20'>Denev</div>
               </div>
             </div>
-            <div className='skeleton h-18 w-full text-center'>
-              <p>Text</p>
-              <p>Text</p>
-              <p>Text</p>
+            <div className='skeleton h-18 w-full text-center pt-2'>
+              <p>Cybersecurity Expert</p>
+              <p>Frontend Developer</p>
+              <p>Backend Developer</p>
               <hr className='w-full border-2' />
               <div className='flex flex-row justify-center my-2'>
-                <button className='btn btn-secondary mx-2'>Call</button>
-                <button className='btn btn-secondary mx-2'>Email</button>
+                <a href='mailto:Tihomir@motion-mate.bg' data-rel='external'>
+                  <button className='btn btn-secondary mx-2'>Email</button>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div id='Rado'>
+        <div id='Radoslav'>
           <div className='flex flex-col gap-4 w-52 mx-4'>
             <div className='flex gap-4 items-center'>
-              <div className='skeleton w-16 h-16 rounded-full shrink-2'></div>
+              <div className='skeleton w-18 h-18 rounded-full shrink-2'>
+                <img src={userPhoto} />
+              </div>
               <div className='flex flex-col gap-4'>
                 <div className='skeleton text-center h-7 w-20'>Radoslav</div>
                 <div className='skeleton text-center h-7 w-20'>Marinov</div>
               </div>
             </div>
-            <div className='skeleton h-18 w-full text-center'>
-              <p>Text</p>
-              <p>Text</p>
-              <p>Text</p>
+            <div className='skeleton h-18 w-full text-center pt-2'>
+              <p>Cybersecurity Expert</p>
+              <p>Frontend Developer</p>
+              <p>Backend Developer</p>
               <hr className='w-full border-2' />
               <div className='flex flex-row justify-center my-2'>
-                <button className='btn btn-secondary mx-2'>Call</button>
-                <button className='btn btn-secondary mx-2'>Email</button>
+                <a href='mailto:Radoslav@motion-mate.bg' data-rel='external'>
+                  <button className='btn btn-secondary mx-2'>Email</button>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div id='Tisho'>
+        <div id='Tanya'>
           <div className='flex flex-col gap-4 w-52 mx-4'>
             <div className='flex gap-4 items-center'>
-              <div className='skeleton w-16 h-16 rounded-full shrink-2'></div>
+              <div className='skeleton w-18 h-18 rounded-full shrink-2'>
+                <img src={userPhoto} />
+              </div>
               <div className='flex flex-col gap-4'>
                 <div className='skeleton text-center h-7 w-20'>Tanya</div>
                 <div className='skeleton text-center h-7 w-20'>Jecheva</div>
               </div>
             </div>
-            <div className='skeleton h-18 w-full text-center'>
-              <p>Text</p>
-              <p>Text</p>
-              <p>Text</p>
+            <div className='skeleton h-18 w-full text-center pt-2'>
+              <p>Cybersecurity Expert</p>
+              <p>Frontend Developer</p>
+              <p>Backend Developer</p>
               <hr className='w-full border-2' />
               <div className='flex flex-row justify-center my-2'>
-                <button className='btn btn-secondary mx-2'>Call</button>
-                <button className='btn btn-secondary mx-2'>Email</button>
+                <a href='mailto:Tanya@motion-mate.bg' data-rel='external'>
+                  <button className='btn btn-secondary mx-2'>Email</button>
+                </a>
               </div>
             </div>
           </div>
         </div>
+        <img src={logo} className='w-60 h-60 mx-4' />
       </div>
       <div className='flex flex-col items-center text-center my-2'>
         <p>
