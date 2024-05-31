d0262bad52f506628fd019c96728c31d22cae817
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-31 17:35:14 +0300

    Add Min and Max range to input info in BMI

--- src/components/BMI/BMIInImperialSystem.jsx
+++ src/components/BMI/BMIInImperialSystem.jsx
@@ -1,4 +1,10 @@
 import PropTypes from 'prop-types';
+import {
+  HEIGHT_MAX_IN,
+  HEIGHT_MIN_IN,
+  WEIGHT_MAX_LBS,
+  WEIGHT_MIN_LBS,
+} from '../../common/constants';
 
 export default function BMIInImperialSystem({ form, updateForm }) {
   return (
@@ -6,20 +12,24 @@ export default function BMIInImperialSystem({ form, updateForm }) {
       <label htmlFor='weight'>Weight in lbs: </label>
       <input
         type='number'
+        min={WEIGHT_MIN_LBS}
+        max={WEIGHT_MAX_LBS}
         name='weight'
         placeholder='mass'
         value={form?.weight}
         onChange={updateForm('weight')}
-        className='border-2 border-gray-500 rounded p-2 mr-6 bg-gray-200 shadow-xl text-black'
+        className='border-2 border-gray-500 rounded p-2 mr-6 bg-gray-200 shadow-xl text-black w-28'
       />
       <label htmlFor='height'>Height in in: </label>
       <input
         type='number'
+        min={HEIGHT_MIN_IN}
+        max={HEIGHT_MAX_IN}
         name='height'
         placeholder='inches'
         value={form?.height}
         onChange={updateForm('height')}
-        className='border-2 border-gray-500 rounded p-2 m-2 bg-gray-200 shadow-xl text-black'
+        className='border-2 border-gray-500 rounded p-2 m-2 bg-gray-200 shadow-xl text-black w-28'
       />
     </div>
   );
--- src/components/BMI/BMIInMetricSystem.jsx
+++ src/components/BMI/BMIInMetricSystem.jsx
@@ -1,4 +1,10 @@
 import PropTypes from 'prop-types';
+import {
+  HEIGHT_MAX,
+  HEIGHT_MIN,
+  WEIGHT_MAX,
+  WEIGHT_MIN,
+} from '../../common/constants';
 
 export default function BMIInMetricSystem({ form, updateForm }) {
   return (
@@ -6,20 +12,24 @@ export default function BMIInMetricSystem({ form, updateForm }) {
       <label htmlFor='weight'>Weight in kg: </label>
       <input
         type='number'
+        min={WEIGHT_MIN}
+        max={WEIGHT_MAX}
         name='weight'
         placeholder='kilograms'
         value={form?.weight}
         onChange={updateForm('weight')}
-        className='border-2 border-gray-500 rounded p-2 mr-6 bg-gray-200 shadow-xl text-black'
+        className='border-2 border-gray-500 rounded p-2 mr-6 bg-gray-200 shadow-xl text-black w-28'
       />
       <label htmlFor='height'>Height in cm: </label>
       <input
         type='number'
+        min={HEIGHT_MIN}
+        max={HEIGHT_MAX}
         name='height'
         placeholder='centimeters'
         value={form?.height}
         onChange={updateForm('height')}
-        className='border-2 border-gray-500 rounded p-2 m-2 bg-gray-200 shadow-xl text-black'
+        className='border-2 border-gray-500 rounded p-2 m-2 bg-gray-200 shadow-xl text-black w-28'
       />
     </div>
   );
