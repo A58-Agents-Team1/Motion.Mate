0d39045e2c322c49e20fbe1c6ab21b326b604d0f
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-06-02 18:49:52 +0300

    Implement Not Found page with custom messages

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,8 +1,61 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+
 export default function NotFound() {
+  const { user } = useContext(AppContext);
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>
-        <p>Not Found</p>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <img
+          src={
+            'https://raw.githubusercontent.com/A58-Agents-Team1/Motion.Mate/main/src/assets/logo.png'
+          }
+          className='w-40 h-40'
+        />
+        <div className='text-center leading-tight font-extrabold'>
+          <h1 className='text-3xl sm:text-4xl lg:text-6xl'>
+            <span>Error 404</span>
+            <br />
+            <span className='text-red-400'>Page Not Found</span>
+          </h1>
+        </div>
+
+        <div className='text-center'>
+          <p className='text-lg'>
+            The page you are looking for might have been removed had its name
+            changed or is temporarily unavailable.
+          </p>
+          {user ? (
+            <>
+              <p className='text-base mt-4'>
+                Why don`t you head back to the home page and explore Motion Mate
+                from there?
+              </p>
+              <button
+                className='btn btn-primary mt-8'
+                onClick={() => navigate('/')}
+              >
+                Home
+              </button>
+            </>
+          ) : (
+            <>
+              <p className='text-base mt-4'>
+                Why don`t you head back to the login page and explore Motion
+                Mate from there?
+              </p>
+              <button
+                className='btn btn-primary mt-8'
+                onClick={() => navigate('/login')}
+              >
+                Login
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
