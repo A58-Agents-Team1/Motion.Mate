e4a4955c9f0d15c083be82f9ccb626463b7785d1
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-06-06 23:13:13 +0300

    Create WellnessAndHealthTools Page in NavBar

---
+++ src/views/WellnessAndHealthTools/WellnessAndHealthTools.jsx
@@ -0,0 +1,11 @@
+import ActivityLevelWaterIntake from '../../components/ActivityLevelWaterIntake/ActivityLevelWaterIntake';
+import BodyMassIndex from '../../components/BMI/BodyMassIndex';
+
+export default function WellnessHealthTools() {
+  return (
+    <>
+      <ActivityLevelWaterIntake />
+      <BodyMassIndex />
+    </>
+  );
+}
