d72419c164cb1120e6d0cc64c3b936ec881aeeac
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-06-07 03:06:39 +0300

    Add progress slider and edit functionality to DetailedGoal component

--- src/components/Goals/CreateGoal.jsx
+++ src/components/Goals/CreateGoal.jsx
@@ -2,7 +2,15 @@ import { useContext, useState } from 'react';
 import { createGoal } from '../../services/goal.service';
 import { AppContext } from '../../context/AppContext';
 import { validateGoalForm } from '../../common/goal.validations';
-import { APP_NAME } from '../../common/constants';
+import {
+  APP_NAME,
+  GOAL_MAX_CALORIES,
+  GOAL_MAX_EXERCISES,
+  GOAL_MAX_PROGRESS,
+  GOAL_MIN_CALORIES,
+  GOAL_MIN_EXERCISES,
+  GOAL_MIN_PROGRESS,
+} from '../../common/constants';
 import AlertSuccess from '../Alerts/AlertSuccess';
 import AlertError from '../Alerts/AlertError';
 import { alertHelper } from '../../helper/alert-helper';
@@ -14,10 +22,15 @@ export default function CreateGoal() {
   const [showError, setShowError] = useState(false);
   const [errorMessage, setAlertMessage] = useState('');
   const [showSuccess, setShowSuccess] = useState(false);
+
   const [form, setForm] = useState({
     name: '',
     from: '',
     to: '',
+    type: 'manual',
+    exercises: GOAL_MIN_EXERCISES,
+    calories: GOAL_MIN_CALORIES,
+    progress: GOAL_MIN_PROGRESS,
   });
 
   const updateForm = (props) => (e) => {
@@ -36,16 +49,25 @@ export default function CreateGoal() {
         new Date(form.from).getTime(),
         new Date(form.to).getTime()
       );
+
       await createGoal(
         form.name,
         userData.username,
         new Date(form.from).getTime(),
-        new Date(form.to).getTime()
+        new Date(form.to).getTime(),
+        form.type,
+        Number(form.exercises),
+        Number(form.progress),
+        Number(form.calories)
       );
       setForm({
         name: '',
         from: '',
         to: '',
+        type: 'manual',
+        exercises: GOAL_MIN_EXERCISES,
+        calories: GOAL_MIN_CALORIES,
+        progress: GOAL_MIN_PROGRESS,
       });
 
       document.getElementById('my_modal_3').close();
@@ -85,7 +107,7 @@ export default function CreateGoal() {
             </button>
           </form>
           <div className='form-control gap-3'>
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className='input input-bordered flex items-center gap-2 shadow-xl'>
               Name
               <input
                 type='text'
@@ -95,25 +117,122 @@ export default function CreateGoal() {
                 onChange={updateForm('name')}
               />
             </label>
-            <label className='input input-bordered flex items-center gap-2'>
-              From
-              <input
-                type='datetime-local'
-                className='w-full'
-                value={form.from}
-                onChange={updateForm('from')}
-              />
-            </label>
+            <div className='join join-vertical shadow-xl'>
+              <label className='input input-bordered flex items-center gap-2'>
+                From
+                <input
+                  type='datetime-local'
+                  className='w-full'
+                  value={form.from}
+                  onChange={updateForm('from')}
+                />
+              </label>
 
-            <label className='input input-bordered flex items-center gap-2'>
-              To
-              <input
-                type='datetime-local'
-                className='w-full'
-                value={form.to}
-                onChange={updateForm('to')}
-              />
-            </label>
+              <label className='input input-bordered flex items-center gap-2'>
+                To
+                <input
+                  type='datetime-local'
+                  className='w-full'
+                  value={form.to}
+                  onChange={updateForm('to')}
+                />
+              </label>
+            </div>
+
+            <label className='label self-center '>Goal Type</label>
+            <div className='flex w-full'>
+              <div className='grid h-20 flex-grow card bg-base-300 rounded-box place-items-center shadow-2xl'>
+                <div className='form-control'>
+                  <label className='label cursor-pointer gap-1'>
+                    <span className='label-text'>Exercises</span>
+                    <input
+                      type='radio'
+                      name='radio-10'
+                      className='radio checked:bg-blue-500'
+                      onChange={() => setForm({ ...form, type: 'exercises' })}
+                      checked={form.type === 'exercises' ? true : false}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className='divider divider-horizontal'>OR</div>
+              <div className='grid h-20 flex-grow card bg-base-300 rounded-box place-items-center shadow-2xl'>
+                <div className='form-control'>
+                  <label className='label cursor-pointer gap-1'>
+                    <span className='label-text'>Manual progress</span>
+                    <input
+                      type='radio'
+                      name='radio-10'
+                      className='radio checked:bg-blue-500'
+                      onChange={() => setForm({ ...form, type: 'manual' })}
+                      checked={form.type === 'manual' ? true : false}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className='divider divider-horizontal'>OR</div>
+              <div className='grid h-20 flex-grow card bg-base-300 rounded-box place-items-center shadow-2xl'>
+                <div className='form-control'>
+                  <label className='label cursor-pointer gap-1'>
+                    <span className='label-text'>Calories</span>
+                    <input
+                      type='radio'
+                      name='radio-10'
+                      className='radio checked:bg-blue-500'
+                      onChange={() => setForm({ ...form, type: 'calories' })}
+                      checked={form.type === 'calories' ? true : false}
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className='mt-2 shadow-xl'>
+              {form.type === 'exercises' && (
+                <label className='input input-bordered flex items-center gap-2'>
+                  Exercises
+                  <input
+                    type='number'
+                    className='grow'
+                    placeholder='Exercises'
+                    value={form.exercises}
+                    min={GOAL_MIN_EXERCISES}
+                    max={GOAL_MAX_EXERCISES}
+                    onChange={updateForm('exercises')}
+                    checked={form.type === 'exercises' ? true : false}
+                  />
+                </label>
+              )}
+              {form.type === 'manual' && (
+                <label className='input input-bordered flex items-center gap-2'>
+                  Progress {form.progress}%
+                  <input
+                    type='range'
+                    className='grow'
+                    placeholder='Progress'
+                    min={GOAL_MIN_PROGRESS}
+                    max={GOAL_MAX_PROGRESS}
+                    value={form.progress}
+                    onChange={updateForm('progress')}
+                    checked={form.type === 'manual' ? true : false}
+                  />
+                </label>
+              )}
+              {form.type === 'calories' && (
+                <label className='input input-bordered flex items-center gap-2'>
+                  Calories
+                  <input
+                    type='number'
+                    className='grow'
+                    placeholder='Calories'
+                    min={GOAL_MIN_CALORIES}
+                    max={GOAL_MAX_CALORIES}
+                    value={form.calories}
+                    onChange={updateForm('calories')}
+                    checked={form.type === 'calories' ? true : false}
+                  />
+                </label>
+              )}
+            </div>
           </div>
           <button
             type='button'
@@ -123,11 +242,7 @@ export default function CreateGoal() {
             Create
           </button>
         </div>
-        {showError && (
-          <AlertError
-            message={`Something went wrong! You forget about the ${errorMessage}... right?!`}
-          />
-        )}
+        {showError && <AlertError message={`${errorMessage}!`} />}
       </dialog>
       {showSuccess && <AlertSuccess message='Goal Created!' />}
     </>
