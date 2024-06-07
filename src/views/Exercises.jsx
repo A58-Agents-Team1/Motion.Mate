e12e87e8613f83d080698122bc9e477166b0dc4f
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-08 00:17:12 +0300

    fix the css in the component

--- src/views/Exercises.jsx
+++ src/views/Exercises.jsx
@@ -95,7 +95,7 @@ export const Exercises = () => {
       <h2 className='text-2xl font-bold mb-4'>
         Exercises for Category: {category}
       </h2>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 '>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
         {exercises.length > 0 ? (
           exercises.map((exercise) => (
             <div
