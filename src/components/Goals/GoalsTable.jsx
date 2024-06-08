ab33de99ee8a846c2d13daf4409d563ff9f669ef
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-06-08 16:33:00 +0300

    Refactor GoalsTable component to separate folders and update data retrieval logic

---
+++ src/components/Goals/GoalsTable.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import Goal from './Goal';
+import { useEffect } from 'react';
+
+const GoalsTable = ({ goals, myGoals = false }) => {
+  useEffect(() => {
+    console.log('goals:', goals);
+  }, [goals]);
+
+  return (
+    <div className='flex flex-col w-full gap-2 overflow-x-auto'>
+      {goals?.length > 0 ? (
+        <table className='table'>
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Goals</th>
+              <th>Type</th>
+              <th>Name</th>
+              <th>Time left</th>
+              <th>Progress</th>
+            </tr>
+          </thead>
+          {/* row */}
+          <tbody>
+            {goals.map((goal, idx) => (
+              <tr
+                key={goal?.id}
+                className={`${idx % 2 === 0 && 'bg-base-200 '}`}
+              >
+                <Goal
+                  id={goal?.id}
+                  owner={goal?.owner}
+                  name={goal?.name}
+                  from={goal?.timePeriod?.from}
+                  type={goal?.type}
+                  to={goal?.timePeriod?.to}
+                  progress={goal?.progress}
+                />
+              </tr>
+            ))}
+          </tbody>
+          {/* foot */}
+          <tfoot>
+            <tr>
+              <th>Goals</th>
+              <th>Type</th>
+              <th>Name</th>
+              <th>Time left</th>
+              <th>Progress</th>
+            </tr>
+          </tfoot>
+        </table>
+      ) : (
+        <div className='mx-auto my-2'>
+          <h2 className='flex justify-center mb-2 text-lg'>No Goals found.</h2>
+          {myGoals && <p>Create the first goal with the button above.</p>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+GoalsTable.propTypes = {
+  goals: PropTypes.array,
+  myGoals: PropTypes.bool,
+};
+export default GoalsTable;
