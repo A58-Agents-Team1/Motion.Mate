d26519a395114513e51fbdbe0267cfe0aa7072e8
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-06-11 00:30:50 +0300

    Update page title to include app name and section name

--- src/views/WellnessAndHealthTools/WellnessAndHealthTools.jsx
+++ src/views/WellnessAndHealthTools/WellnessAndHealthTools.jsx
@@ -1,8 +1,12 @@
+import { APP_NAME } from '../../common/constants';
 import ActivityLevelWaterIntake from '../../components/ActivityLevelWaterIntake/ActivityLevelWaterIntake/ActivityLevelWaterIntake';
 import BodyMassIndex from '../../components/BMI/BodyMassIndex/BodyMassIndex';
 import CalorieCalculator from '../../components/CalorieCalculator/CalorieCalculator/CalorieCalculator';
 
 export default function WellnessHealthTools() {
+  document.querySelector(
+    'title'
+  ).textContent = `${APP_NAME} | Wellness & Health Tools`;
   return (
     <div>
       <ActivityLevelWaterIntake />
