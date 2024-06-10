0557f609de6c28e030a335368332ef346936a800
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-10 17:27:37 +0300

    fix the bug when mapping empty snapshot

--- src/components/HomeAuth/FinishedGoal.jsx
+++ src/components/HomeAuth/FinishedGoal.jsx
@@ -13,10 +13,11 @@ export const FinishedGoal = () => {
 
   useEffect(() => {
     return onValue(
-      ref(db, `users/${userData.username}/myGoals`),
+      ref(db, `users/${userData?.username}/myGoals`),
+
       (snapshot) => {
         try {
-          if (snapshot) {
+          if (snapshot.val()) {
             const goalsWithId = Object.entries(snapshot?.val()).map(
               ([id, goal]) => ({
                 id,
@@ -40,61 +41,67 @@ export const FinishedGoal = () => {
 
   return (
     <>
-      {goals?.map((goal) => (
-        <div key={goal.id}>
-          <div className='flex flex-col items-center'>
-            <div className='card w-96 bg-base-300 m-2 shadow-xl'>
-              <div className='card-body p-4'>
-                <div className='flex justify-between items-center'>
-                  <h2 className='card-title text-lg font-bold'>Goal</h2>
-                  <h3 className='text-lg font-bold'>{goal.name}</h3>
-                  <div className='flex space-x-2'>
-                    <FontAwesomeIcon
-                      icon={faStar}
-                      className='text-yellow-500'
-                    />
-                    <FontAwesomeIcon
-                      icon={faMedal}
-                      className='text-blue-500'
-                    />
+      {goals ? (
+        <>
+          {goals?.map((goal) => (
+            <div key={goal.id}>
+              <div className='flex flex-col items-center'>
+                <div className='card w-96 bg-base-300 m-2 shadow-xl'>
+                  <div className='card-body p-4'>
+                    <div className='flex justify-between items-center'>
+                      <h2 className='card-title text-lg font-bold'>Goal</h2>
+                      <h3 className='text-lg font-bold'>{goal.name}</h3>
+                      <div className='flex space-x-2'>
+                        <FontAwesomeIcon
+                          icon={faStar}
+                          className='text-yellow-500'
+                        />
+                        <FontAwesomeIcon
+                          icon={faMedal}
+                          className='text-blue-500'
+                        />
+                      </div>
+                    </div>
+
+                    <div className='grid grid-cols-2 gap-2 '>
+                      <InfoBite
+                        isBlock
+                        title={'Duration'}
+                        content={`From : ${new Date(
+                          goal?.timePeriod?.from
+                        ).toLocaleString()}`}
+                      />
+
+                      <InfoBite
+                        isBlock
+                        content={`To: ${new Date(
+                          goal?.timePeriod?.to
+                        ).toLocaleString()}`}
+                      />
+
+                      <InfoBite
+                        title={'Calories'}
+                        content={goal.calories}
+                      />
+                    </div>
+
+                    <div className='card-actions justify-end mt-4'>
+                      <button
+                        onClick={() => removeGoalAsync(userData, goal)}
+                        className='btn btn-secondary'
+                      >
+                        Remove
+                      </button>
+                    </div>
                   </div>
-                </div>
-
-                <div className='grid grid-cols-2 gap-2 '>
-                  <InfoBite
-                    isBlock
-                    title={'Duration'}
-                    content={`From : ${new Date(
-                      goal?.timePeriod?.from
-                    ).toLocaleString()}`}
-                  />
-
-                  <InfoBite
-                    isBlock
-                    content={`To: ${new Date(
-                      goal?.timePeriod?.to
-                    ).toLocaleString()}`}
-                  />
-
-                  <InfoBite
-                    title={'Calories'}
-                    content={goal.calories}
-                  />
-                </div>
-
-                <div className='card-actions justify-end mt-4'>
-                  <button
-                    onClick={() => removeGoalAsync(userData, goal)}
-                    className='btn btn-secondary'
-                  >
-                    Remove
-                  </button>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      ))}
+          ))}
+        </>
+      ) : (
+        <>Finish goals to see them here!</>
+      )}
     </>
   );
 };
