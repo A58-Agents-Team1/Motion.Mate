d537bb577215c7bdb3ea63aaec8b58c20e566223
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-11 00:50:38 +0300

    made only user owners to delete exercises

--- src/views/Exercises/Exercises.jsx
+++ src/views/Exercises/Exercises.jsx
@@ -200,51 +200,53 @@ export const Exercises = () => {
                       </button>
                     )}
                     {exercise.createdBy === userData.username && (
-                      <button
-                        onClick={() =>
-                          startEditing(
-                            exercise,
-                            setEditingExerciseId,
-                            setEditForm
-                          )
-                        }
-                        className='btn btn-outline btn-primary'
-                      >
-                        Edit
-                      </button>
+                      <>
+                        <button
+                          onClick={() =>
+                            startEditing(
+                              exercise,
+                              setEditingExerciseId,
+                              setEditForm
+                            )
+                          }
+                          className='btn btn-outline btn-primary'
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={() =>
+                            handleDelete(
+                              category,
+                              exercise.id,
+                              deleteExercise,
+                              alertHelper,
+                              setAlertMessage,
+                              setShowSuccess,
+                              setShowError,
+                              'Exercise deleted!'
+                            )
+                          }
+                          className='btn btn-outline btn-secondary'
+                        >
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='h-6 w-6'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                          >
+                            <path
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
+                              strokeWidth='2'
+                              d='M6 18L18 6M6 6l12 12'
+                            />
+                          </svg>
+                        </button>
+                      </>
                     )}
                   </div>
                 )}
-                <button
-                  onClick={() =>
-                    handleDelete(
-                      category,
-                      exercise.id,
-                      deleteExercise,
-                      alertHelper,
-                      setAlertMessage,
-                      setShowSuccess,
-                      setShowError,
-                      'Exercise deleted!'
-                    )
-                  }
-                  className='btn btn-outline btn-secondary'
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-6 w-6'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M6 18L18 6M6 6l12 12'
-                    />
-                  </svg>
-                </button>
               </div>
             </div>
           ))
