aff4555c3bd2a7b230bf5938e15fc8b4a34f12e6
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-11 15:02:05 +0300

    added an if statement if there is no title provided in the InfoByte

--- src/components/Exercise/InfoBite.jsx
+++ src/components/Exercise/InfoBite.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 const InfoBite = ({ title, content, isBlock }) => {
   const flexClass = isBlock ? '' : 'flex items-center';
+  const hasTitle = title ? `${title}:` : '';
 
   return (
     <div className={`${flexClass} border border-primary p-1 px-2 rounded-md`}>
-      <strong className='mr-1 text-primary'>{title}:</strong>
+      <strong className='mr-1 text-primary'>{hasTitle}</strong>
 
       <p>{content}</p>
     </div>
