694edb4b19fd42bdc055d25d86467bdd8cb4be40
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-06-11 21:09:23 +0300

    Refactor Goals component to have live update on friends goals

--- src/views/Goals/Goals.jsx
+++ src/views/Goals/Goals.jsx
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useState } from 'react';
-import CreateGoal from '../../components/Goals/CreateGoal';
 import { APP_NAME } from '../../common/constants';
 import { ref, onValue } from 'firebase/database';
 import { db } from '../../config/firebase-config';
 import { AppContext } from '../../context/AppContext';
-import { getGoals } from '../../services/goal.service';
+import CreateGoal from '../../components/Goals/CreateGoal';
 import GoalsTable from '../../components/Goals/GoalsTable';
 
 export default function Goals() {
@@ -13,37 +12,53 @@ export default function Goals() {
   const { userData } = useContext(AppContext);
 
   const [goals, setGoals] = useState([]);
+  const [friends, setFriends] = useState([]);
   const [friendsGoals, setFriendsGoals] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  const friendGoalsMap = new Map();
 
   useEffect(() => {
     return onValue(ref(db, `users/${userData?.username}`), (snapshot) => {
       const _goals = [];
-      const _friendsGoals = [];
       const myGoals = snapshot.val()?.myGoals;
-      const friends = snapshot.val()?.friends;
+      const friends = Object.keys(snapshot.val()?.friends);
+
+      setFriends(friends);
 
       for (const key in myGoals) {
         _goals.push({
           id: key,
           ...snapshot.val()?.myGoals[key],
         });
+        setGoals(_goals);
       }
-      for (const friend in friends) {
-        getGoals(friend)
-          .then((friendGoals) => {
-            friendGoals.forEach((goal) => {
-              _friendsGoals.push(goal);
-            });
-          })
-          .then(() => {
-            !friendsGoals.includes(_friendsGoals?.id) &&
-              setFriendsGoals(_friendsGoals);
-          });
-      }
-
-      setGoals(_goals);
     });
   }, []);
+
+  useEffect(() => {
+    if (!friends) return;
+    if (friends.length === 0) return;
+    if (counter > friends.length) {
+      friendGoalsMap.clear();
+      setFriendsGoals([]);
+      setCounter(0);
+    }
+    friends?.map((friend) => {
+      return onValue(ref(db, `users/${friend}`), (snapshot) => {
+        const myGoals = snapshot.val()?.myGoals;
+
+        for (const key in myGoals) {
+          friendGoalsMap.set(key, {
+            id: key,
+            ...snapshot.val()?.myGoals[key],
+          });
+        }
+        setFriendsGoals([...friendGoalsMap.values()]);
+        setCounter((prev) => prev + 1);
+      });
+    });
+  }, [friends]);
 
   return (
     <div className='flex flex-col w-full gap-2  overflow-x-auto'>
