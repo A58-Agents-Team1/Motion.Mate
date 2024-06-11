ae672c5e6572ebbf5701a50aee7a01fb94d6771e
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-11 12:43:46 +0300

    fix timer with 2 seconds

--- src/helper/exercise-timer.js
+++ src/helper/exercise-timer.js
@@ -2,7 +2,9 @@ export const handleOnStart = async (exercise, startExercise, userData) => {
   const endDate = new Date();
   endDate.setHours(endDate.getHours() + Number(exercise.duration.hours));
   endDate.setMinutes(endDate.getMinutes() + Number(exercise.duration.minutes));
-  endDate.setSeconds(endDate.getSeconds() + Number(exercise.duration.seconds));
+  endDate.setSeconds(
+    endDate.getSeconds() + Number(exercise.duration.seconds) + 2
+  );
 
   await startExercise(
     userData.username,
