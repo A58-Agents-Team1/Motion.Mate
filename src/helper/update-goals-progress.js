94c2ef2ed634cf0988fd30b756ffb64d48d9fd06
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-06-11 15:09:48 +0300

    Update updateGoalProgressCalories function to handle goal progress calculation and marking as done

--- src/helper/update-goals-progress.js
+++ src/helper/update-goals-progress.js
@@ -12,22 +12,28 @@ export const updateGoalProgressCalories = async (username, calories) => {
     for (const goal of allGoals.filter(
       (goal) => goal?.type === 'calories' && !goal?.isDone
     )) {
+      const today = new Date().getTime();
       if (calories) {
-        await updateGoalCalories(username, goal, goal?.id, calories);
+        if (
+          goal?.timePeriod?.from <= today &&
+          goal?.timePeriod?.to >= today
+        ) {
+          await updateGoalCalories(username, goal, goal?.id, calories);
 
-        (await getGoals(username)).map(async (goal) => {
-          let calculate = Math.floor(
-            (goal.caloriesBurned / goal.calories) * 100
-          );
+          (await getGoals(username)).map(async (goal) => {
+            let calculate = Math.floor(
+              (goal.caloriesBurned / goal.calories) * 100
+            );
 
-          if (calculate > 100) {
-            calculate = 100;
-          }
-          await updateGoalProgress(username, goal?.id, calculate);
-          if (goal.progress === 100) {
-            await markAsDone(username, goal.id);
-          }
-        });
+            if (calculate > 100) {
+              calculate = 100;
+            }
+            await updateGoalProgress(username, goal?.id, calculate);
+            if (goal.progress === 100) {
+              await markAsDone(username, goal.id);
+            }
+          });
+        }
       }
     }
   }
