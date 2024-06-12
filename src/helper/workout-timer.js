773e28f86043bf4e693a35b09474181838f888c7
TanyaZhecheva <jechevatanq@gmail.com>
2024-06-12 15:00:36 +0300

    add 2 seconds to timer to work properly

--- src/helper/workout-timer.js
+++ src/helper/workout-timer.js
@@ -7,7 +7,7 @@ export const workoutTimer = async (
   const endDate = new Date();
   endDate.setHours(endDate.getHours() + Number(content.hours));
   endDate.setMinutes(endDate.getMinutes() + Number(content.minutes));
-  endDate.setSeconds(endDate.getSeconds() + Number(content.seconds));
+  endDate.setSeconds(endDate.getSeconds() + Number(content.seconds) + 2);
   await StartTimerWorkout(userData.username, endDate.getTime());
   setContent({
     hours: 0,
